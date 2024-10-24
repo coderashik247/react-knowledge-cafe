282c75e1bd447fa69b1f0250579c960ea99bc6e4
Ashik <ashikurashik20999@gmail.com>
2024-10-24 15:21:30 +0600

    second commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-6xl font-bold bg-red-400'>Knowledge Cafe4</h1>
+        </div>
+    );
+};
+
+export default Header;
